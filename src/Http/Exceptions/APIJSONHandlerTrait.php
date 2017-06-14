bd13fd21235ea10cfd9fe09bc05a0b82d6017655
evgenyl <lexunin@gmail.com>
2017-06-14 22:03:54 +0300

    Error handling.

---
+++ src/Http/Exceptions/APIJSONHandlerTrait.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * REST API Core
+ *
+ * @author    Evgeny Leksunin <e.leksunin@gmail.com>
+ * @copyright 2017 Evgeny Leksunin
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      https://github.com/evgeny-l/rest-api-core
+ */
+
+namespace EvgenyL\RestAPICore\Http\Exceptions;
+
+use EvgenyL\RestAPICore\Http\Responses\FormattedJSONResponse;
+use Exception;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+/**
+ * Class APIJSONHandlerTrait
+ * @package EvgenyL\RestAPICore\Http\Exceptions
+ */
+class APIJSONHandlerTrait
+{
+
+    public function handleJSONResponse($request, Exception $exception)
+    {
+        $exception = $this->prepareException($exception);
+        $data = [];
+        $message = $exception->getMessage();
+        $code = $exception->getCode();
+        if ($exception instanceof HttpException) {
+            $code = $exception->getStatusCode();
+        }
+        elseif ($exception instanceof ValidationException) {
+            $code = 422;
+            $data = $exception->validator->errors()->getMessages();
+        } elseif ($exception instanceof AuthenticationException) {
+            $code = 401;
+        } elseif ($exception instanceof AuthorizationException) {
+            $code = 403;
+        }
+        if ($code < 100 || $code >= 600) {
+            $code = 500;
+        } elseif ($code == 405) {
+            $message = 'Method not allowed.';
+        }
+        return FormattedJSONResponse::error($code, $message, $data);
+    }
+
+}
